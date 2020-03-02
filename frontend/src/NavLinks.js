0e9f2ff7d5453f03cfd15098cb2bbeaddad016fe
backcoc <yryousafraza@gmail.com>
2020-03-02 14:57:31 +0500

    ishtiaqs modifications

---
+++ frontend/src/NavLinks.js
@@ -0,0 +1,33 @@
+/**
+ *  header-menu and sidebar menu data
+ */
+export default [
+   {
+      "menu_title": "Home",
+      "path": "/",
+      "icon": "home",
+      
+   },
+   {
+      "menu_title": "Shop",
+      "path": "/shop",
+      "mega": true,
+      "icon": "party_mode",
+
+      
+   },
+   
+
+   {
+      "menu_title": "Compare brands",
+      "path": "/compare",
+      "icon": "home",
+      
+   },
+   {
+      "menu_title": "Admin Panel",
+      "path": "/admin-panel/Reports",
+      "icon": "perm_identity",
+      "child_routes": null
+   }
+]
